09b79d99f058d2cdb88383bf79669595ee0539f0
gangbs <yangyangangil@163.com>
2019-12-01 12:21:10 +0800

    aop组件封装

---
+++ Super.Core/Super.Core.Infrastruct/AOP/DecoratorBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Super.Core.Infrastruct.AOP
+{
+    public class DecoratorBuilder<T> where T : class//T必须为一个抽象的接口
+    {
+        private T _component;
+        private object _proxy;
+
+        public DecoratorBuilder(T component)
+        {
+            this._component = component;
+        }
+
+        public T BuilderDecorator()
+        {
+            return this.BuilderDecorator(null, null);
+        }
+
+        public T BuilderDecorator(Action<MethodInfo, object[]> beforeAction, Action<MethodInfo, object[], object> afterAction)
+        {
+            _proxy = DispatchProxy.Create<T, DecoratorProxy<T>>();
+            ((DecoratorProxy<T>)_proxy).Wrapped = this._component;
+            ((DecoratorProxy<T>)_proxy).BeforeAction = beforeAction;
+            ((DecoratorProxy<T>)_proxy).AfterAction = afterAction;
+            return (T)_proxy;
+        }
+
+        public void AddBeforeAction(Action<MethodInfo, object[]> action)
+        {
+            ((DecoratorProxy<T>)_proxy).BeforeAction += action;
+        }
+        public void RemoveBeforeAction(Action<MethodInfo, object[]> action)
+        {
+            ((DecoratorProxy<T>)_proxy).BeforeAction -= action;
+        }
+
+        public void AddAfterAction(Action<MethodInfo, object[], object> action)
+        {
+            ((DecoratorProxy<T>)_proxy).AfterAction += action;
+        }
+        public void RemoveAfterAction(Action<MethodInfo, object[], object> action)
+        {
+            ((DecoratorProxy<T>)_proxy).AfterAction -= action;
+        }
+
+    }
+}
---
+++ Super.Core/Super.Core.Infrastruct/AOP/DecoratorProxy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Super.Core.Infrastruct.AOP
+{
+    public class DecoratorProxy<T> : DispatchProxy
+    {
+        public T Wrapped;
+        public Action<MethodInfo, object[]> BeforeAction;   //动作之前执行
+        public Action<MethodInfo, object[], object> AfterAction;   //动作之后执行
+
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            if (this.BeforeAction != null)
+                this.BeforeAction(targetMethod, args);
+
+            var result = targetMethod.Invoke(this.Wrapped, args);
+
+            if (this.AfterAction != null)
+                this.AfterAction(targetMethod, args, result);
+
+            return result;
+        }
+
+
+    }
+}
