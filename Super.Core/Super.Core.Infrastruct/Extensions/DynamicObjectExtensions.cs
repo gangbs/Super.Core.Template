8a5444334c60a3b2b6cef0c9fb214789b3252f04
gangbs <yangyangangil@163.com>
2019-12-07 11:16:18 +0800

    动态对象扩展方法封装

---
+++ Super.Core/Super.Core.Infrastruct/Extensions/DynamicObjectExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace Super.Core.Infrastruct.Extensions
+{
+    public static class DynamicObjectExtensions
+    {
+        public static Dictionary<string, object> ToDictionary(this ExpandoObject dyn)
+        {
+            var dic = dyn.ToDictionary(x => x.Key, x => x.Value);
+            return dic;
+        }
+
+        public static T ToObject<T>(this ExpandoObject dyn) where T : class
+        {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(dyn);
+            var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+            return obj;
+        }
+
+        public static List<T> ToObject<T>(this List<ExpandoObject> lstDyn) where T : class
+        {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(lstDyn);
+            var lst = Newtonsoft.Json.JsonConvert.DeserializeObject<List<T>>(json);
+            return lst;
+        }
+
+    }
+}
